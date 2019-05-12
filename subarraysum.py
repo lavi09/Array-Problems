f641ff5b4780f77fd8cf92909e83561a76bb70be
lavi09 <42918272+lavi09@users.noreply.github.com>
2019-05-12 10:33:05 -0700

    Add files via upload

--- subarraysum.py
+++ subarraysum.py
@@ -16,26 +16,26 @@ def subArraySum(arr, n, Sum):
        
         # add current element to curr_sum  
         curr_sum = curr_sum + arr[i]  
-    
+        
         # if curr_sum is equal to target sum  
         # we found a subarray starting from index 0  
         # and ending at index i  
         if curr_sum == Sum:  
            
             print("Sum found between indexes 0 to", i) 
-            return 
+            
            
-    
+        print(curr_sum-Sum)
         # If curr_sum - sum already exists in map  
         # we have found a subarray with target sum  
         if (curr_sum - Sum) in Map:  
-           
+            
             print("Sum found between indexes",Map[curr_sum - Sum] + 1, "to", i)  
               
-            return 
+            return 1
     
         Map[curr_sum] = i  
-    
+        print("m",Map[curr_sum])
     # If we reach here, then no subarray exists  
     print("No subarray with given sum exists")  
    
@@ -43,8 +43,9 @@ def subArraySum(arr, n, Sum):
 # Driver program to test above function  
 if __name__ == "__main__":  
    
-    arr = [-10,0,2,-2,-20,10]  
+    arr = [10,2,-2,-20,10]  
     n = len(arr)  
-    Sum = 20
+    Sum = -10
+    
     
     subArraySum(arr, n, Sum)  
